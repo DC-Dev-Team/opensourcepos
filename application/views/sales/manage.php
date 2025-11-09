68e8fa5ffdd1ed81330c32434a1fad4c81d8d106
Eric Adedayo <adedayo.ae@gmail.com>
2025-11-09 12:15:27 +0100

    This feature includes the selection of branch daily sales and also correct summary
    This feature includes the selection of branch daily sales and also correct summary
    
    This feature includes the selection of branch daily sales and also correct summary

--- application/views/sales/manage.php
+++ application/views/sales/manage.php
@@ -8,6 +8,11 @@ $(document).ready(function()
 		table_support.refresh();
 	});
 	
+	// when location dropdown changes
+	$('#location_filter').on('change', function(e) {
+		table_support.refresh();
+	});
+	
 	// load the preset datarange picker
 	<?php $this->load->view('partial/daterangepicker'); ?>
 
@@ -22,6 +27,7 @@ $(document).ready(function()
 		return {
 			start_date: start_date,
 			end_date: end_date,
+			location_id: $("#location_filter").val() || 'all',
 			filters: $("#filters").val() || [""]
 		}
 	};
@@ -32,8 +38,12 @@ $(document).ready(function()
 		pageSize: <?php echo $this->config->item('lines_per_page'); ?>,
 		uniqueId: 'sale_id',
 		onLoadSuccess: function(response) {
-			if($("#table tbody tr").length > 1) {
+			// Always update payment summary when data loads
+			if(response.payment_summary) {
 				$("#payment_summary").html(response.payment_summary);
+			}
+			
+			if($("#table tbody tr").length > 1) {
 				$("#table tbody tr:last td:first").html("");
 				$("#table tbody tr:last").css('font-weight', 'bold');
 			}
@@ -66,6 +76,11 @@ $(document).ready(function()
 		</button>
 
 		<?php echo form_input(array('name'=>'daterangepicker', 'class'=>'form-control input-sm', 'id'=>'daterangepicker')); ?>
+		
+		<?php if (!empty($stock_locations)): ?>
+			<?php echo form_dropdown('location_filter', $stock_locations, 'all', array('id'=>'location_filter', 'class'=>'selectpicker show-menu-arrow', 'data-style'=>'btn-default btn-sm', 'data-width'=>'fit')); ?>
+		<?php endif; ?>
+		
 		<?php echo form_multiselect('filters[]', $filters, '', array('id'=>'filters', 'data-none-selected-text'=>$this->lang->line('common_none_selected_text'), 'class'=>'selectpicker show-menu-arrow', 'data-selected-text-format'=>'count > 1', 'data-style'=>'btn-default btn-sm', 'data-width'=>'fit')); ?>
 	</div>
 </div>
